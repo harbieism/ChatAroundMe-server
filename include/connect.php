da7c17acc11686ef9590dca5b76d73b80f2087de
harbieism <jow598@utulsa.edu>
2014-10-25 15:58:42 -0500

    Changed the host name in the connect script.

--- include/connect.php
+++ include/connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("104.131.13.110", "root", "bigguccisosa", "chatstorm");
+$mysqli = new mysqli("localhost", "root", "bigguccisosa", "chatstorm", 3306);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
